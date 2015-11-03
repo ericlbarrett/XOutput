f45ac74af5aaa6665bd82b2618fac9532f494b1c
Eric Barrett <b.ericlynn@gmail.com>
2015-11-03 15:43:01 -0800

    Groundwork for new config save format

--- XOutput/ControllerOptions.cs
+++ XOutput/ControllerOptions.cs
@@ -15,33 +15,44 @@ namespace XOutput
             int ind = 0;
 
             foreach (MultiLevelComboBox m in this.Controls.OfType<MultiLevelComboBox>()) {
-                m.Items[0] = getBindingText(ind);
-                m.addOption("None");
-                m.addOption("Detect");
+                //Tag structure: [Type, Number, Index]
+                m.Items[0] = getBindingText(ind); //Change combobox text according to saved binding
+                m.addOption("None",
+                    tag: new byte[] { 255, 0, (byte)ind });
+                m.addOption("Detect",
+                    tag: new byte[] { 254, 0, (byte)ind });
                 ToolStripMenuItem axes = m.addMenu("Axes");
                 ToolStripMenuItem buttons = m.addMenu("Buttons");
                 ToolStripMenuItem dpads = m.addMenu("D-Pads");
                 ToolStripMenuItem iaxes = m.addMenu("Inverted Axes", axes);
                 ToolStripMenuItem haxes = m.addMenu("Half Axes", axes);
                 ToolStripMenuItem ihaxes = m.addMenu("Inverted Half Axes", axes);
-                //Tag structure: [Type, Number, Index]
                 for (int i = 1; i <= dev.joystick.Capabilities.ButtonCount; i++)
                 {
-                    m.addOption("Button " + i.ToString(), buttons, new byte[] { 0, (byte)(i - 1), (byte)ind });
+                    m.addOption("Button " + i.ToString(), buttons,
+                        new byte[] { 0, (byte)(i - 1), (byte)ind });
                 }
                 for (int i = 1; i <= dev.joystick.Capabilities.PovCount; i++)
                 {
-                    m.addOption("D-Pad " + i.ToString() + " Up", dpads, new byte[] { 32, (byte)(i - 1), (byte)ind });
-                    m.addOption("D-Pad " + i.ToString() + " Down", dpads, new byte[] { 33, (byte)(i - 1), (byte)ind });
-                    m.addOption("D-Pad " + i.ToString() + " Left", dpads, new byte[] { 34, (byte)(i - 1), (byte)ind });
-                    m.addOption("D-Pad " + i.ToString() + " Right", dpads, new byte[] { 35, (byte)(i - 1), (byte)ind });
+                    m.addOption("D-Pad " + i.ToString() + " Up", dpads,
+                        new byte[] { 32, (byte)(i - 1), (byte)ind });
+                    m.addOption("D-Pad " + i.ToString() + " Down", dpads,
+                        new byte[] { 33, (byte)(i - 1), (byte)ind });
+                    m.addOption("D-Pad " + i.ToString() + " Left", dpads,
+                        new byte[] { 34, (byte)(i - 1), (byte)ind });
+                    m.addOption("D-Pad " + i.ToString() + " Right", dpads,
+                        new byte[] { 35, (byte)(i - 1), (byte)ind });
                 }
                 for (int i = 1; i <= dev.joystick.Capabilities.AxesCount; i++)
                 {
-                    m.addOption("Axis " + i.ToString(), axes, new byte[] { 16, (byte)(i - 1), (byte)ind });
-                    m.addOption("IAxis " + i.ToString(), iaxes, new byte[] { 17, (byte)(i - 1), (byte)ind });
-                    m.addOption("HAxis" + i.ToString(), haxes, new byte[] { 18, (byte)(i - 1), (byte)ind });
-                    m.addOption("IHAxis" + i.ToString(), ihaxes, new byte[] { 19, (byte)(i - 1), (byte)ind });
+                    m.addOption("Axis " + i.ToString(), axes,
+                        new byte[] { 16, (byte)(i - 1), (byte)ind });
+                    m.addOption("IAxis " + i.ToString(), iaxes,
+                        new byte[] { 17, (byte)(i - 1), (byte)ind });
+                    m.addOption("HAxis" + i.ToString(), haxes,
+                        new byte[] { 18, (byte)(i - 1), (byte)ind });
+                    m.addOption("IHAxis" + i.ToString(), ihaxes,
+                        new byte[] { 19, (byte)(i - 1), (byte)ind });
                 }
                 m.SelectionChangeCommitted += new System.EventHandler(SelectionChanged);
                 ind++;
@@ -58,8 +69,7 @@ namespace XOutput
             byte num = (byte)(dev.mapping[(i * 2) + 1] + 1);
             string s = "";
 
-            switch (type)
-            {
+            switch (type) {
                 case 0:
                     return "Button " + num.ToString();
                 case 1:
@@ -95,6 +105,9 @@ namespace XOutput
         private void SelectionChanged(object sender, EventArgs e) {
             ToolStripMenuItem i = (ToolStripMenuItem)sender;
             byte[] b = (byte[])i.Tag;
+            if (b[0] == 254) {
+                
+            }
             dev.mapping[b[2] * 2] = b[0];
             dev.mapping[(b[2] * 2) + 1] = b[1];
             dev.Save();
--- XOutput/SaveManager.cs
+++ XOutput/SaveManager.cs
@@ -5,7 +5,10 @@ namespace XOutput
 {
     static class SaveManager
     {
-
+        static string[] names = new string[] {"button_a", "button_b", "button_x", "button_y",
+            "dpad_up", "dpad_down", "dpad_left", "dpad_right",
+            "left_trigger", "right_trigger", "left_bumper", "right_bumper", "left_axebutton", "right_axebutton",
+            "home", "start", "back", "left_y", "left_x", "right_y", "right_x"};
         static private string fileName = @"XOutput.ini";
 
 
@@ -52,12 +55,61 @@ namespace XOutput
             return null;
         }
 
+        private static string generateSaveString(byte[] Mapping) {
+            string saveString = "";
+            for (int i = 0; i < 21; i++) {
+                saveString = saveString + names[i] + "=";
+                if (Mapping[i * 2] == 255) {
+                    saveString = saveString + "disabled\r\n";
+                    continue;
+                }
+                byte subType = (byte)(Mapping[i * 2] & 0x0F);
+                byte type = (byte)((Mapping[i * 2] & 0xF0) >> 4);
+                byte num = (byte)(Mapping[i * 2 + 1] + 1);
+                switch (type) {
+                    case 0: //Buttons
+                        saveString = saveString + "btn" + num.ToString() + "\r\n";
+                        break;
+                    case 1: //Axes
+                        string typeStr = "axis";
+                        switch (subType) {
+                            case 1:
+                                typeStr = "i" + typeStr;
+                                break;
+                            case 2:
+                                typeStr = "h" + typeStr;
+                                break;
+                            case 3:
+                                typeStr = "ih" + typeStr;
+                                break;
+                        }
+                        saveString = saveString + typeStr + num.ToString() + "\r\n";
+                        break;
+                    case 2: //D-Pads
+                        switch (subType) {
+                            case 0:
+                                saveString = saveString + "dpad_" + num.ToString() + "_up\r\n";
+                                break;
+                            case 1:
+                                saveString = saveString + "dpad_" + num.ToString() + "_down\r\n";
+                                break;
+                            case 2:
+                                saveString = saveString + "dpad_" + num.ToString() + "_left\r\n";
+                                break;
+                            case 3:
+                                saveString = saveString + "dpad_" + num.ToString() + "_right\r\n";
+                                break;
+                        }
+                        break;
+                }
+            }
+            return saveString;
+        }
 
         public static void Save(string _Guid, byte[] Mapping)
         {
-            string saveString;
-
-
+            string saveString = generateSaveString(Mapping);
+            Console.WriteLine(saveString);
             string s = "";
             if (File.Exists(fileName))
             {
