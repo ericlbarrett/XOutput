f565e64b37d107090fb4efcf1c7604d4a3ebd033
Eric Barrett <Eric Barrett>
2015-11-04 00:25:55 -0800

    Work on new mapping save format

--- XOutput/SaveManager.cs
+++ XOutput/SaveManager.cs
@@ -3,51 +3,89 @@ using System.IO;
 
 namespace XOutput
 {
-    static class SaveManager
-    {
+    static class SaveManager {
         static string[] names = new string[] {"button_a", "button_b", "button_x", "button_y",
             "dpad_up", "dpad_down", "dpad_left", "dpad_right",
             "left_trigger", "right_trigger", "left_bumper", "right_bumper", "left_axebutton", "right_axebutton",
             "home", "start", "back", "left_y", "left_x", "right_y", "right_x"};
         static private string fileName = @"XOutput.ini";
 
-
-        public static byte[] Load(string _Guid)
-        {
-            TextReader mappingFile = null;
-            if (File.Exists(fileName))
-            {
-                try
-                {
-
-                    mappingFile = new StreamReader(fileName);
-                    string strLine = mappingFile.ReadLine();
-                    while (strLine != null)
-                    {
-                        if (strLine != "")
-                        {
-                            strLine = strLine.Trim();
-                            if (strLine.StartsWith("[") && strLine.EndsWith("]"))
-                            {
-                                string Guid = strLine.Substring(1, strLine.Length - 2);
-                                if (Guid == _Guid)
-                                {
-                                    strLine = mappingFile.ReadLine();
-                                    return Convert.FromBase64String(strLine);
-                                }
-                            }
-                        }
-                        strLine = mappingFile.ReadLine();
-                    }
-                    mappingFile.Close();
-                }
-                catch (Exception ex)
-                {
-                    throw ex;
+        private static void parseLine(string line) {
+            int i; //The index of the control in the map array
+            byte type = 255, subType = 255, num = 255;
+            for (i = 0; i < 21; i++) {
+                if (line.StartsWith(names[i])) {
+                    break;
                 }
             }
-            else
-            {
+            string val = line.Remove(0, names[i].Length + 1);
+            if (val.StartsWith("btn")) {
+                type = 0;
+                subType = 0;
+                num = byte.Parse(val.Remove(0, 3));
+            } else if (val.Contains("axis")) {
+                type = 1;
+                if (val.StartsWith("ih")) {
+                    num = byte.Parse(val.Remove(0, 6));
+                    subType = 3;
+                } else if (val.StartsWith("h")) {
+                    subType = 2;
+                    num = byte.Parse(val.Remove(0, 5));
+                } else if (val.StartsWith("i")) {
+                    subType = 1;
+                    num = byte.Parse(val.Remove(0, 5));
+                } else {
+                    subType = 0;
+                    num = byte.Parse(val.Remove(0, 4));
+                }
+            } else if (val.StartsWith("dpad")) {
+                type = 2;
+                val = val.Remove(0, 4);
+                if (val.EndsWith("up")) {
+                    subType = 0;
+                    num = byte.Parse(val.Remove(val.Length - 2));
+                } else if (val.EndsWith("down")) {
+                    subType = 1;
+                    num = byte.Parse(val.Remove(val.Length - 4));
+                } else if (val.EndsWith("left")) {
+                    subType = 2;
+                    num = byte.Parse(val.Remove(val.Length - 4));
+                } else if (val.EndsWith("right")) {
+                    subType = 3;
+                    num = byte.Parse(val.Remove(val.Length - 4));
+                }
+            } else if (val == "disabled") {
+            }
+            Console.WriteLine(type);
+            Console.WriteLine(subType);
+            Console.WriteLine(num);
+
+        }
+
+        public static byte[] Load(string _Guid) {
+            bool parsing = false;
+            if (File.Exists(fileName)) {
+                TextReader mappingFile = new StreamReader(fileName);
+                string strLine = mappingFile.ReadLine();
+                while (strLine != null) {
+                    strLine = strLine.Trim();
+                    if (strLine != "") {
+                        if (strLine.StartsWith("[") && strLine.EndsWith("]")) {
+                            if (parsing) {
+                                break;
+                            }
+                            string Guid = strLine.Substring(1, strLine.Length - 2);
+                            if (Guid == _Guid) {
+                                parsing = true;
+                            }
+                        } else if (parsing) {
+                            parseLine(strLine);
+                        }
+                    }
+                    strLine = mappingFile.ReadLine();
+                }
+                mappingFile.Close();
+            } else {
                 TextWriter tw = new StreamWriter(fileName);
                 tw.Write("");
                 tw.Close();
@@ -55,11 +93,53 @@ namespace XOutput
             return null;
         }
 
+        public static void Save(string _Guid, byte[] Mapping) {
+            string saveString = generateSaveString(Mapping);
+            string fileString = "";
+            if (File.Exists(fileName)) {
+
+                TextReader mappingFile = new StreamReader(fileName);
+                string strLine = mappingFile.ReadLine();
+                bool saved = false;
+                bool noWrite = false;
+
+                while (strLine != null) {
+                    strLine = strLine.Trim(); //Trim gets rid of line endings
+                    if (strLine != "") {
+                        if (strLine.StartsWith("[") && strLine.EndsWith("]")) {
+                            noWrite = false;
+                            string Guid = strLine.Substring(1, strLine.Length - 2);
+                            if (Guid == _Guid) {
+                                fileString += strLine + "\r\n";
+                                fileString += saveString;
+                                saved = true;
+                                noWrite = true;
+                            }
+                        }
+                        if (!noWrite)
+                            fileString += strLine + "\r\n";
+                    }
+                }
+                mappingFile.Close();
+
+                if (!saved) {
+                    fileString += "[" + _Guid + "]" + "\r\n";
+                    fileString += saveString;
+                }
+            } else {
+                fileString += "[" + _Guid + "]" + "\r\n";
+                fileString += saveString;
+            }
+            TextWriter tw = new StreamWriter(fileName, false);
+            tw.Write(fileString);
+            tw.Close();
+        }
+
         private static string generateSaveString(byte[] Mapping) {
             string[] typeString = new string[] { "btn{0}", "{1}axis{0}", "dpad{0}{2}" };
             string[] axesString = new string[] { "", "i", "h", "ih" };
             string[] dpadString = new string[] { "up", "down", "left", "right" };
-            
+
             string saveString = "";
             for (int i = 0; i < 21; i++) {
                 saveString += names[i] + "=";
@@ -73,62 +153,6 @@ namespace XOutput
                 saveString += string.Format(typeString[type], num, axesString[subType], dpadString[subType]) + "\r\n";
             }
             return saveString;
-        }
-
-        public static void Save(string _Guid, byte[] Mapping)
-        {
-            string saveString = generateSaveString(Mapping);
-            Console.WriteLine(saveString);
-            string s = "";
-            if (File.Exists(fileName))
-            {
-                try
-                {
-                    string[] fl = File.ReadAllLines(fileName);
-                    string strLine;
-                    bool saved = false;
-
-
-                    for (int i = 0; i < fl.Length; i++)
-                    {
-                        strLine = fl[i];
-                        if (strLine != "")
-                        {
-                            strLine = strLine.Trim();
-                            if (strLine.StartsWith("[") && strLine.EndsWith("]"))
-                            {
-                                string Guid = strLine.Substring(1, strLine.Length - 2);
-                                if (Guid == _Guid)
-                                {
-                                    s += strLine + "\r\n";
-                                    s += Convert.ToBase64String(Mapping) + "\r\n";
-                                    saved = true;
-                                    break;
-                                }
-                            }
-                        }
-                        s += strLine + "\r\n";
-                    }
-
-                    if (!saved)
-                    {
-                        s += ("[" + _Guid + "]" + "\r\n");
-                        s += Convert.ToBase64String(Mapping) + "\r\n";
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
-            }
-            else
-            {
-                s += ("[" + _Guid + "]" + "\r\n");
-                s += Convert.ToBase64String(Mapping) + "\r\n";
-            }
-            TextWriter tw = new StreamWriter(fileName, false);
-            tw.Write(s);
-            tw.Close();
         }
 
     }
